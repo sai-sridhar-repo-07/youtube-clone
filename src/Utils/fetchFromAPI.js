15249c389eb6194e5702fcda7ea92abaf7c0f1c7
sai-sridhar-repo-07 <tarrasridhar1154@gmail.com>
2023-10-26 05:51:54 +0530

    Feed page completed

---
+++ src/Utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    // "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": "dbdcc6ea74msh3794934cb31cb5ep1d7cf9jsn09c99d4daa77",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
